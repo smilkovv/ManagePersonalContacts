e5850675ab2782c79d7a0f9d6d948ea36c9edcf0
smilkovv <103314204+smilkovv@users.noreply.github.com>
2022-04-09 14:06:27 +0300

    Add files via upload

---
+++ ManagingPersonalContacts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManagingPersonalContacts.Startup))]
+namespace ManagingPersonalContacts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
